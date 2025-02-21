8f44fccd552d0935c79bd29d20ea0067f2da1e3a
Ruben <ruben.velasco2004@gmail.com>
2025-02-21 17:20:23 +0100

    Add dto files

---
+++ src/main/java/org/example/grandao/dto/Ejemplar.java
@@ -0,0 +1,66 @@
+package org.example.grandao.dto;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "ejemplar")
+public class Ejemplar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "isbn", nullable = false)
+    private Libro isbn;
+
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "estado", nullable = false, length = 100)
+    private String estado;
+
+    @OneToMany(mappedBy = "ejemplar")
+    private Set<Prestamo> prestamos = new LinkedHashSet<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Libro getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Libro isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Set<Prestamo> getPrestamos() {
+        return prestamos;
+    }
+
+    public void setPrestamos(Set<Prestamo> prestamos) {
+        this.prestamos = prestamos;
+    }
+
+}
